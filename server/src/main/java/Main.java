9ae82d0868ae9715b39698a0f8ea1bef94d7df9f
aearl23 <64229908+aearl23@users.noreply.github.com>
2024-10-19 20:53:47 -0600

    added server test

--- server/src/main/java/Main.java
+++ server/src/main/java/Main.java
@@ -1,8 +1,18 @@
 import chess.*;
+import server.Server;
 
 public class Main {
     public static void main(String[] args) {
         var piece = new ChessPiece(ChessGame.TeamColor.WHITE, ChessPiece.PieceType.PAWN);
         System.out.println("♕ 240 Chess Server: " + piece);
+
+        //create server object
+        Server server = new Server();
+        //start server on port 8080
+        int port = 8081;
+        int actualPort = server.run(port);
+
+        //check server start
+        System.out.println("Server started on port: " + actualPort);
     }
 }
