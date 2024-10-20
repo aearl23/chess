02e09d977d75dabccd8acb551ae48a802c039303
aearl23 <64229908+aearl23@users.noreply.github.com>
2024-10-20 16:21:26 -0600

    add UserService update

--- server/src/main/java/service/UserService.java
+++ server/src/main/java/service/UserService.java
@@ -15,17 +15,35 @@ public class UserService {
     this.dataAccess = dataAccess;
   }
   public AuthData register(UserData user) throws DataAccessException {
-      if(dataAccess.getUser(user.username()) != null){
+    //check if user already exists
+    if(dataAccess.getUser(user.username()) != null){
         throw new DataAccessException("Error: username already taken");
       }
+      //create user
       dataAccess.createUser(user);
 
-      String authToken = UUID.randomUUID().toString();
-      AuthData authData = new AuthData(authToken, user.username());
-      dataAccess.createAuth(authData);
-
-      return authData;
+      //generate authToken
+      return createAuthToken(user.username());
   }
-  public AuthData login(UserData user) {}
-  public void logout(AuthData auth) {}
+  public AuthData login(String username, String password) throws DataAccessException {
+    UserData user = dataAccess.getUser(username);
+    if (user == null || !user.password().equals(password)){
+      throw new DataAccessException("Error: unauthorized");
+    }
+    return createAuthToken(username);
+  }
+  public void logout(String authToken) throws DataAccessException{
+    if (dataAccess.getAuth(authToken) == null) {
+      throw new DataAccessException("Error: unauthorized");
+    }
+    dataAccess.deleteAuth(authToken);
+  }
+
+  private AuthData createAuthToken(String username) throws DataAccessException{
+    //generate authToken
+    String authToken = UUID.randomUUID().toString();
+    AuthData authData = new AuthData(authToken, username);
+    dataAccess.createAuth(authData);
+    return authData;
+  }
 }
