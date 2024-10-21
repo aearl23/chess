6daabdd20cef26f84a6e18cda1094ef55920845e
aearl23 <64229908+aearl23@users.noreply.github.com>
2024-10-21 11:54:24 -0600

    add endpoints to server class

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -1,16 +1,27 @@
 package server;
 
 import spark.*;
+import com.google.gson.Gson;
 import dataaccess.DataAccess;
 import dataaccess.MemoryDataAccess;
 import service.UserService;
+import service.GameService;
+import service.AdminService;
+import java.util.List;
 
 public class Server {
     private final UserService userService;
+    private final GameService gameService;
+    private final AdminService adminService;
+
+    pirvate final Gson gson;
 
     public Server(){
         DataAccess dataAccess = new MemoryDataAccess();
         this.userService = new UserService(dataAccess);
+        this.gameService = new GameService(dataAccess);
+        this.adminService = new AdminService(dataAccess);
+        this.gson = new Gson();
     }
 
     public int run(int desiredPort) {
@@ -23,7 +34,19 @@ public class Server {
         //This line initializes the server and can be removed once you have a functioning endpoint
 
         //Add endpoints here
-        Spark.post("/user", new "RegisterHandler"(userService));
+        Spark.post("/user", (req, res) -> {
+            var user = gson.fromJson(req.body(), model.UserData.class);
+            var auth = userService.register(user);
+            res.status(200);
+            return gson.toJson(auth);
+        });
+
+        Spark.post("/session", (req, res) -> {
+           var loginRequest = gson.fromJson(req.body(), LoginRequest.class);
+           var auth = userService.login(loginRequest.username(), loginRequest.password());
+           res.status(200);
+           return gson.toJson(auth);
+        });
 
         Spark.awaitInitialization();
         return Spark.port();
@@ -33,4 +56,12 @@ public class Server {
         Spark.stop();
         Spark.awaitStop();
     }
+
+    private record LoginRequest(String username, String password) {}
+    private record CreateGameRequest(String gameName) {}
+    private record JoinGameRequest(String playerColor, int gameID) {}
+    private record CreateGameResponse(int gameID) {}
+    private record ListGameResponse(List<model.GameData> games) {}
+    private record ErrorResponse(String message) {}
+
 }
---
+++ server/src/main/java/service/GameService.java
@@ -0,0 +1,61 @@
+package service;
+
+import dataaccess.DataAccess;
+import dataaccess.DataAccessException;
+import model.GameData;
+import model.AuthData;
+import chess.ChessGame;
+import java.util.List;
+
+public class GameService {
+  private final DataAccess dataAccess;
+
+  public GameService(DataAccess dataAccess) {
+    this.dataAccess = dataAccess;
+  }
+
+  public List<GameData> listGames(String authToken) throws DataAccessException {
+    if (dataAccess.getAuth(authToken) == null) {
+      throw new DataAccessException("Error: unauthorized");
+    }
+    return dataAccess.listGames();
+  }
+
+  public int createGame(String authToken, String gameName) throws DataAccessException {
+    if (dataAccess.getAuth(authToken) == null) {
+      throw new DataAccessException("Error: unauthorized");
+    }
+    GameData newGame = new GameData(0, null, null, gameName, new ChessGame());
+    return dataAccess.createGame(newGame);
+  }
+
+  public void joinGame(String authToken, String playerColor, int gameID) throws DataAccessException {
+    AuthData auth = dataAccess.getAuth(authToken);
+    if (auth == null) {
+      throw new DataAccessException("Error: unauthorized");
+    }
+
+    GameData game = dataAccess.getGame(gameID);
+    if (game == null) {
+      throw new DataAccessException("Error: bad request");
+    }
+
+    String username = auth.username();
+    if (playerColor.equalsIgnoreCase("WHITE")) {
+      if (game.whiteUsername() != null) {
+        throw new DataAccessException("Error: already taken");
+      }
+      game = new GameData(game.gameID(), username, game.blackUsername(), game.gameName(), game.game());
+    } else if (playerColor.equalsIgnoreCase("BLACK")) {
+      if (game.blackUsername() != null) {
+        throw new DataAccessException("Error: already taken");
+      }
+      game = new GameData(game.gameID(), game.whiteUsername(), username, game.gameName(), game.game());
+    } else {
+      throw new DataAccessException("Error: bad request");
+    }
+
+    dataAccess.updateGame(game);
+  }
+}
+
